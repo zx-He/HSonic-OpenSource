db9bfb421cd2070c81328c98f8837dde3b039943
zx.He <41406833+zx-He@users.noreply.github.com>
2024-08-07 17:32:02 +0800

    BANN迁移学习数据和代码

--- README.md
+++ README.md
@@ -6,20 +6,17 @@ the dataset and source code of paper "HeadSonic: Reliable Bone Conduction Earpho
 
 Dataset：
 
-​	Download from : https://www.kaggle.com/datasets/miracle0723/hsonic-dataset
-
-​    Data from 45 subjects(subject ID = folder name), each subject is required to collect one session data (100 samples) at each of the four different behaviors, i.e., sitting on a chair (chair_10), rotating the head (head_10), rotating the body (body_10), and walking (walk_10).
+​    Data from 45 subjects, each subject is required to collect one session data (100 samples) at each of the four different behaviors, i.e., sitting on a chair (chair.pth), rotating the head (head.pth), rotating the body (body.pth), and walking (walk.pth).
 
 ​	Each sample has a length of 3072 (output of the SCAE model).
 
 ​	
 
-
-
 Source code: 
 
-​	Run "main.py" to train and test the BANN model. 
+​	Run "BANN_transfer.py" to for the BANN model transfer learning. 
 
-​	"main.py" automatically calls code from other files, sequentially designating each subject as a legitimate user for evaluation.
+​	"BANN_transfer.py" automatically calls code from other files, sequentially designating each subject as a legitimate user for evaluation.
 
   More details could be seen within the source code.
+
--- model.py
+++ model.py
@@ -17,61 +17,88 @@ class ViT(nn.Module):
         return x
 
 
-class BANNModel(nn.Module):
-
+class FeatureExtractor(nn.Module):
     def __init__(self):
-        super(BANNModel, self).__init__()
+        super(FeatureExtractor, self).__init__()
+        self.feature = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2),
+            nn.ReLU(True),
+            nn.Conv2d(64, 50, kernel_size=3, padding=1),
+            nn.BatchNorm2d(50),
+            nn.Dropout2d(),
+            nn.MaxPool2d(2),
+            nn.ReLU(True)
+        )
 
-        self.feature = nn.Sequential()
+    def forward(self, x):
+        return self.feature(x).view(-1, 8, 50*8)
 
-        self.feature.add_module('f_conv1', nn.Conv2d(3, 64, kernel_size=3, padding=1))
-        self.feature.add_module('f_bn1', nn.BatchNorm2d(64))
-        self.feature.add_module('f_pool1', nn.MaxPool2d(2))
-        self.feature.add_module('f_relu1', nn.ReLU(True))
-        self.feature.add_module('f_conv2', nn.Conv2d(64, 50, kernel_size=3, padding=1))
-        self.feature.add_module('f_bn2', nn.BatchNorm2d(50))
-        self.feature.add_module('f_drop1', nn.Dropout2d())
-        self.feature.add_module('f_pool2', nn.MaxPool2d(2))
-        self.feature.add_module('f_relu2', nn.ReLU(True))
-
-
+class UserClassifier(nn.Module):
+    def __init__(self):
+        super(UserClassifier, self).__init__()
         self.gru1 = nn.GRU(input_size=400, hidden_size=400, num_layers=2, batch_first=True)
         self.mlp11 = nn.Linear(400, 100)
         self.BN11 = nn.BatchNorm1d(100)
         self.relu11 = nn.ReLU(True)
         self.mlp12 = nn.Linear(100, 2)
 
+    def forward(self, x):
+        x, _ = self.gru1(x)
+        x = x[:, -1, :] #output the last time step data
+        x = self.mlp11(x)
+        x = self.BN11(x)
+        x = self.relu11(x)
+        x = self.mlp12(x)
+        return F.log_softmax(x, dim=1)
 
+class BehaviorClassifier(nn.Module):
+    def __init__(self):
+        super(BehaviorClassifier, self).__init__()
         self.transformerEncoder = ViT(latentVecSize=400)
         self.mlp21 = nn.Linear(400, 100)
         self.BN21 = nn.BatchNorm1d(100)
         self.relu21 = nn.ReLU(True)
         self.mlp22 = nn.Linear(100, 4)
 
+    def forward(self, x):
+        x = self.transformerEncoder(x)
+        x = x[:, 0] #output the first position data
+        x = self.mlp21(x)
+        x = self.BN21(x)
+        x = self.relu21(x)
+        x = self.mlp22(x)
+        return F.log_softmax(x, dim=1)
+
+class BANNModel(nn.Module):
+    def __init__(self):
+        super(BANNModel, self).__init__()
+        self.feature_extractor = FeatureExtractor()
+        self.user_classifier = UserClassifier()
+        self.behavior_classifier = BehaviorClassifier()
 
     def forward(self, input_data, lamuda):
-
         input_data = input_data.view(-1, 3, 32, 32)
-        feature = self.feature(input_data)
-        feature = feature.view(-1, 8, 50*8)
+        feature = self.feature_extractor(input_data)
+
+        class_output = self.user_classifier(feature)
+
         reverse_feature = ReverseLayerF.apply(feature, lamuda)
-
-        # User classifier
-        x1, _ = self.gru1(feature)
-        x1 = x1[:, -1, :] #output the last time step data
-        x1 = self.mlp11(x1)
-        x1 = self.BN11(x1)
-        x1 = self.relu11(x1)
-        x1 = self.mlp12(x1)
-        class_output = F.log_softmax(x1, dim=1)
-
-        # Behavior classifier
-        x2 = self.transformerEncoder(reverse_feature)
-        x2 = x2[:, 0] #output the first position data
-        x2 = self.mlp21(x2)
-        x2 = self.BN21(x2)
-        x2 = self.relu21(x2)
-        x2 = self.mlp22(x2)
-        domain_output = F.log_softmax(x2, dim=1)
+        domain_output = self.behavior_classifier(reverse_feature)
 
         return class_output, domain_output
+
+
+class BANNModel_transfer(nn.Module):
+    def __init__(self):
+        super(BANNModel_transfer, self).__init__()
+        self.feature_extractor = FeatureExtractor()
+        self.user_classifier = UserClassifier()
+
+    def forward(self, input_data):
+        input_data = input_data.view(-1, 3, 32, 32)
+        feature = self.feature_extractor(input_data)
+        class_output = self.user_classifier(feature)
+
+        return class_output
